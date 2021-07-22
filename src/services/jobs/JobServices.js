1bed3c1c68c8953b3823f82bf1f74a7991dc35ef
Kruti <krutikachhadiya78@gmail.com>
2021-07-22 14:08:10 +0530

    get jobs using express

--- src/services/jobs/JobServices.js
+++ src/services/jobs/JobServices.js
@@ -4,9 +4,26 @@ export async function getAllJobs() {
 
     try {
 
+        // Request options to get jobs
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                companySkills: true,
+                dismissedListingHashes: [],
+                fetchJobDesc: true,
+                jobTitle: "Business Analyst",
+                locations: [],
+                numJobs: 20,
+                previousListingHashes: []
+            })
+        };
+        const response = await fetch('https://www.zippia.com/api/jobs/', requestOptions)
+            .then(async response => await response.json());
+
         // Calls jobs api from node
-        const response = await fetch('http://localhost:9000/test/jobs')
-            .then(async response => { return await response.json() })
+        // const response = await fetch('http://localhost:9000/test/jobs')
+        //     .then(async response => { return await response.json() })
 
         return await response;
     } catch (error) {
